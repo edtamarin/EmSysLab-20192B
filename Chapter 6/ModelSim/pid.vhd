4e56041b58ea1361de94965e91c5dbbf25107204
Edian <e_dian@live.nl>
2020-06-22 13:08:04 +0200

    Added some comments

--- Chapter 6/ModelSim/pid.vhd
+++ Chapter 6/ModelSim/pid.vhd
@@ -4,33 +4,33 @@ use ieee.numeric_std.all;
 
 entity pid is
     port (
-        kp      :   IN  std_logic;
-        ki      :   IN  std_logic;
-        kd      :   IN  std_logic;
-        pid_in  :   IN  std_logic_vector(7 downto 0);
-        feedback:   IN  std_logic_vector(7 downto 0);
+        kp      :   IN  std_logic; --Proportional on/off
+        ki      :   IN  std_logic; --Integral on/off
+        kd      :   IN  std_logic; --Derivative on/off
+        pid_in  :   IN  std_logic_vector(7 downto 0); --Reference input
+        feedback:   IN  std_logic_vector(7 downto 0); --Feedback from motor
         rst     :   IN  std_logic;
         clk     :   IN  std_logic;
-        pid_out  :   OUT std_logic_vector(7 downto 0)
+        pid_out  :   OUT std_logic_vector(7 downto 0) --PID output
     );
 end pid;
 
 architecture rtl of pid is
     
-constant con_Kp :   integer := 5;
-constant con_kp_den : integer := 2;
-constant con_Kd : integer := 1;
-constant con_kd_den : integer := 100;
-constant con_Ki : integer := 1;
-constant con_ki_den : integer := 10;
-signal error, error_difference, error_sum, previous_error : integer := 0;
-signal p, i, d : integer := 0;
-signal output_loaded, output_saturation_buffer : integer := 0;
-signal previous_feedback : std_logic_vector(7 downto 0);
-constant timedivisor : integer := 1;
+    constant con_Kp :   integer := 5; --Proportional value
+    constant con_kp_den : integer := 2; --Constant
+    constant con_Kd : integer := 1; --Derivative value
+    constant con_kd_den : integer := 100; --Constant
+    constant con_Ki : integer := 1; --Integral value
+    constant con_ki_den : integer := 10; --Constant
+    signal error, error_difference, error_sum, previous_error : integer := 0;
+    signal p, i, d : integer := 0;
+    signal output_loaded, output_saturation_buffer : integer := 0;
+    signal previous_feedback : std_logic_vector(7 downto 0);
+    constant timedivisor : integer := 1;
 
-begin
-    process(kp, kd, ki, clk, rst, feedback, error, pid_in, p, i, d)
+
+    begin process(kp, kd, ki, clk, rst, feedback, error, pid_in, p, i, d)
     begin
         if rising_edge(clk) then
             if rst = '0' then
@@ -43,7 +43,7 @@ begin
                 output_loaded <= 0;
                 pid_out <= (others => '0');
             else
-                for k in 0 to 8 loop
+                for k in 0 to 8 loop --Going through all the PID controller stages with a loop
                     case k is
                         when 0 => 
                             error <= (to_integer(unsigned(pid_in)) - to_integer(unsigned(feedback)));
